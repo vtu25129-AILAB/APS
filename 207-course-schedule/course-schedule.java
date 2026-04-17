cbe6791e0943377e79e961996b74dc76f9df9c05
SRINU_RACHAMALLU <vtu25129@veltech.edu.in>
2026-04-17 10:00:18 +0530

    Time: 7 ms (60.50%) | Memory: 47 MB (66.60%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            count++;
+
+            for (int next : graph.get(course)) {
+                indegree[next]--;
+                if (indegree[next] == 0) q.offer(next);
+            }
+        }
+
+        return count == numCourses;
+    }
+}
